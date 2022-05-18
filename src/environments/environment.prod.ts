3f94e8ed30e9569d0a2eca56a3655519170768dd
Manuel Hernández <carlosmanuel.hernandez@bosonit.com>
2022-05-18 11:03:10 +0200

    Feat: Mapas en Angular working

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoiY21hbnVlbGgiLCJhIjoiY2wzOXh2M25pMDA4NDNkbnJ4bm1neHpoYSJ9.2VRZ2i3MxGhB0VA_0xEkLg'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxToken: 'pk.eyJ1IjoiY21hbnVlbGgiLCJhIjoiY2wzOXh2M25pMDA4NDNkbnJ4bm1neHpoYSJ9.2VRZ2i3MxGhB0VA_0xEkLg'
 };
 
 /*
